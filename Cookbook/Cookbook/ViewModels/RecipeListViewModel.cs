c239546b3bb6f309df7388d432c9d4d2dea5e91e
DESKTOP-1S3G401\Boris <tirkajla.b@hotmail.com>
2022-03-21 20:58:29 +0100

    Problem with binding

--- Cookbook/Cookbook/ViewModels/RecipeListViewModel.cs
+++ Cookbook/Cookbook/ViewModels/RecipeListViewModel.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Cookbook.ViewModels
 {
     internal class RecipeListViewModel : BaseViewModel 
     {
+        private string _mealName;
+
+        public RecipeListViewModel()
+        {
+            MealName = "Dinner";    
+            BackButtonClicked = new Command(OnBackButtonClicked);
+        }
+
+        public string MealName
+        {
+            get => _mealName;
+            set
+            {
+                _mealName = value;
+                OnPropertyChanged(nameof(MealName));
+            } 
+        }
+
+        public ICommand BackButtonClicked { get; }
+
+        private void OnBackButtonClicked()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }

f402455f13548e37662fef8c26b7fb544fc69587
nemanja <nemanjapajcin@gmail.com>
2022-04-08 17:39:27 +0200

    Fonts added

--- Cookbook/Cookbook/AssemblyInfo.cs
+++ Cookbook/Cookbook/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("LeckerliOne-Regular.ttf", Alias="Regular")]
+[assembly: ExportFont("Pattaya-Regular.ttf", Alias = "Regular1")]
+[assembly: ExportFont("MarcellusSC-Regular.ttf", Alias = "Regular2")] 
